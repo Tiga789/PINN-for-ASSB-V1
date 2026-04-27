c7e80a3fa11ac65dd65d916aeb039e5cbf0629a5
Tiga789 <1669016683@qq.com>
2026-04-27 16:28:37 +0800

    更新项目：精简版本

--- exec_noOpt.sh
+++
@@ -1,7 +0,0 @@
-dataFolder=./integration_spm/
-
-rm -r Model*
-rm -r Log*
-
-
-python main.py -nosimp -df $dataFolder -i input
--- exec_repeat.sh
+++
@@ -1,4 +0,0 @@
-dataFolder=./integration_spm/
-rm -r Model*
-rm -r Log*
-python repeat_sim.py -nosimp -opt -df $dataFolder -i input
